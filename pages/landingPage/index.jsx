e88a3a553594d4d2732b01ea2b59aad707e9c7d7
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-15 21:23:45 +0530

    ST01 - routes - added required global routes and basic ui for copyCollection page

--- pages/landingPage/index.jsx
+++ pages/landingPage/index.jsx
@@ -1,15 +1,25 @@
 //
 
 import React from "react";
+// libs
+import { Link } from "react-router-dom";
+// paths
+import { APP_PAGES } from "../../routes/paths";
 
 // ----------------------------------------------
 
 export default function LandingPage() {
   return (
-    <div style={{ padding: 12, borderBottom: "1px solid #E6E6E6" }}>
-      <div>
-        <p>LandingPage</p>
+    <React.Fragment>
+      <div style={{ padding: 12, borderBottom: "1px solid #E6E6E6" }}>
+        <div>
+          <p>LandingPage</p>
+        </div>
       </div>
-    </div>
+
+      <div style={{ padding: 8 }}>
+        <Link to={APP_PAGES.copyCollection}>Copy Collection</Link>
+      </div>
+    </React.Fragment>
   );
 }
