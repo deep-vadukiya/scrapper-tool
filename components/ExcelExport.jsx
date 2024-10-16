207ef00ec4885cdc7d37b156f6d90d7147486070
Deep Vadukiya <dipvadukiya06@gmail.com>
2024-10-16 17:57:11 +0530

    ST04 - excel export - filtered database record and download excel file

--- components/ExcelExport.jsx
+++ components/ExcelExport.jsx
@@ -1,6 +1,11 @@
 //
 
 import React, { useState } from "react";
+// libs
+import * as XLSX from "xlsx";
+// indexDB
+import { getAllIndexDBRecords } from "../indexDB/utilityFunc";
+import { INDEX_DB_CONFIG } from "../indexDB/configDB";
 
 // ----------------------------------------------
 
@@ -9,6 +14,53 @@ export default function ExcelExport() {
 
   const handleInputValues = (e) => {
     setFromToData({ ...fromToData, [e.target.name]: Number(e.target.value) });
+  };
+
+  const exportToExcel = async () => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+
+    if (dd < 10) dd = "0" + dd;
+    if (mm < 10) mm = "0" + mm;
+
+    const formattedToday = dd + "_" + mm + "_" + yyyy;
+
+    const fileName = `lead_enquiry_${formattedToday}_from_${fromToData.from}_to_${fromToData.to}.xlsx`;
+
+    const indexDBData = await getAllIndexDBRecords(
+      INDEX_DB_CONFIG.leadEnquiries.storeObject
+    );
+
+    const filteredIndexDB = indexDBData.slice(
+      fromToData.from - 1,
+      fromToData.to
+    );
+
+    const excelData = [];
+    for (let index = 0; index < filteredIndexDB?.length; index++) {
+      const element = filteredIndexDB[index];
+
+      const data = {
+        ID: element.id,
+        ContactName: element.contacts_name,
+        ContactMobile: element.contacts_mobile,
+        City: element.contact_city,
+        State: element.contact_state,
+        Country: element.country_name,
+      };
+
+      excelData.push(data);
+    }
+
+    // Convert the data array to a worksheet
+    const worksheet = XLSX.utils.json_to_sheet(excelData);
+    // Create a new workbook and add the worksheet
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Lead Enquiries");
+    // Export the workbook to an Excel file
+    XLSX.writeFile(workbook, fileName);
   };
 
   return (
@@ -31,7 +83,10 @@ export default function ExcelExport() {
             onChange={handleInputValues}
           />
 
-          <button disabled={!fromToData.from || !fromToData.to}>
+          <button
+            disabled={!fromToData.from || !fromToData.to}
+            onClick={exportToExcel}
+          >
             Export Excel sheet
           </button>
         </div>
